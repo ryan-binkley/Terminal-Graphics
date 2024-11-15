91bfb00f21700030969be1fc4616e72d66542f46
Ryan Binkley <binkley62918@gmail.com>
2024-11-15 16:02:21 -0700

    In a working state.

--- test/test_double_buffer.c
+++ test/test_double_buffer.c
@@ -1,19 +1,30 @@
 #include <stdlib.h>
 
 #include "unity.h"
-#include "cmock.h"
 #include "double_buffer.h"
 #include "terminal_utils.h"
+#include "mock_terminal_utils.h"
 #include "errors.h"
 
 
-// Test Case: initDoubleBuffer_Success
 void test_initDoubleBuffer_Success(void)
 {
     // Arrange
+    DoubleBuffer* doubleBuffer = calloc(1, sizeof(DoubleBuffer));
+
+    void modify_return_values_getTerminalSize(int* width, int* height)
+    {
+        *width = 10;
+        *height = 10;
+    }
+    getTerminalSize_fake.custom_fake = modify_return_values_getTerminalSize;
 
     // Act
+    initDoubleBuffer(doubleBuffer);
 
     // Assert
-    TEST_ASSERT_EQUAL(3, 3);
+    TEST_ASSERT_EQUAL(1, getTerminalSize_fake.call_count);
+
+    // Cleanup
+    freeDoubleBuffer(doubleBuffer);
 }
